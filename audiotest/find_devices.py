97f78500149a881ad9ea2dcec1d429bd59e686c8
Ethan Klassen <ethan.klassen@ucalgary.ca>
2026-06-15 16:29:42 -0600

    AUDIO stream ONLY, no video files or tests

--- audiotest/find_devices.py
+++
@@ -1,9 +0,0 @@
-import av.option
-
-# Use PyAV's internal library to force a device scan dump to your console
-try:
-    av.open(file="audio=dummy", format="dshow", options={"list_devices": "true"})
-except Exception:
-    # This block is expected to fail with an I/O error, 
-    # but it will print the device list to your terminal right before it crashes!
-    pass
